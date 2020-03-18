6c0497551301ddc65b8c84b40f86fb0e3275feed
Pedro Baumann <ondoheer@gmail.com>
2020-03-18 15:43:52 -0500

    avances de servicios

---
+++ src/js/index.js
@@ -0,0 +1,20 @@
+window.onscroll = function () { scrollFunction() };
+
+/**
+ * Things that happen as you scroll down or up
+ * 1. Header rezises
+ * 2.  toggles To the top button
+ */
+function scrollFunction() {
+    if (document.body.scrollTop > 105 || document.documentElement.scrollTop > 105) {
+        // Header
+        document.getElementById("logo").style.width = "140px";
+        // to the top button
+        document.getElementById("toTheTop").style.display = "block";
+    } else {
+        // header
+        document.getElementById("logo").style.width = "200px";
+        // to the top button
+        document.getElementById("toTheTop").style.display = "none";
+    }
+} 
