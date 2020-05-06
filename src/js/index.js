a1e0a17077f0e4c3e839805ec1d2015339aa1cea
ondoheer <ondoheer@gmail.com>
2020-05-06 15:35:07 -0500

    slider casi listo falta icofonts

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
-window.onscroll = function () { scrollFunction() };
+window.onscroll = function () {
+  scrollFunction();
+};
 
 /**
  * Things that happen as you scroll down or up
@@ -6,15 +8,20 @@ window.onscroll = function () { scrollFunction() };
  * 2.  toggles To the top button
  */
 function scrollFunction() {
-    if (document.body.scrollTop > 105 || document.documentElement.scrollTop > 105) {
-        // Header
-        document.getElementById("logo").style.width = "140px";
-        // to the top button
-        document.getElementById("toTheTop").style.display = "block";
-    } else {
-        // header
-        document.getElementById("logo").style.width = "200px";
-        // to the top button
-        document.getElementById("toTheTop").style.display = "none";
-    }
-} 
+  if (
+    document.body.scrollTop > 105 ||
+    document.documentElement.scrollTop > 105
+  ) {
+    // Header
+    document.getElementById("logo").style.width = "140px";
+    // to the top button
+    document.getElementById("toTheTop").style.display = "block";
+  } else {
+    // header
+    document.getElementById("logo").style.width = "200px";
+    // to the top button
+    document.getElementById("toTheTop").style.display = "none";
+  }
+}
+
+$(".c-carousel").slick({ dots: true });
